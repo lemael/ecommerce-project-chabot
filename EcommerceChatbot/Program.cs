f9f1a2f4a8b194617440447a8ecae856276d88af
lemael <tchindefossomael@yahoo.fr>
2025-08-13 10:42:26 -0600

    update Program.cs4

--- EcommerceChatbot/Program.cs
+++ EcommerceChatbot/Program.cs
@@ -18,7 +18,8 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("http://localhost:3000"
+        ,"https://ecommerce-project-2kvd.onrender.com")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
     });
